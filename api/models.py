054c313085e04ca04936c91cb0b62d08c4cc959a
Carlos Arruda <caugustogarruda@gmail.com>
2018-08-26 17:02:06 -0300

    Alteração retorno metodo __str__ e alteração do related no voto

--- api/models.py
+++ api/models.py
@@ -11,14 +11,14 @@ class Produto(models.Model):
     cadatrado_em = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.nome_produto
+        return self.produto
 
 class Voto(models.Model):
     """
         autor: Carlos Arruda
         criado em: 08/2018
     """
-    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='produto')  
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE, related_name='voto')  
     nota = models.IntegerField()
     ponto_positivo = models.CharField(max_length=255,null=True, blank=True)
     ponto_negativo = models.CharField(max_length=255,null=True, blank=True)  
