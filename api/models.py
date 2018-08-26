8322358f555286b3ae6a0334d8a72ac97272e4a3
Carlos Arruda <caugustogarruda@gmail.com>
2018-08-26 08:51:03 -0300

    Criando o model ususario, adição cpffield e git ignore

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cpffield import cpffield
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=255)
+    cpf_cnpj = cpffield.CPFField('CPF', max_length=14, unique=True)
+    email = models.EmailField(max_length=255, unique=True)
+    senha = models.CharField(max_length=255)
+    cadatrado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(null=True)
+    
+    def __str__(self):
+        return self.nome
