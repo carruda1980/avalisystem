d449928d4783b0d8483ce2679bb4c48909d64d3d
Carlos Arruda <caugustogarruda@gmail.com>
2018-09-01 08:48:25 -0300

    Adição url admin ao projeto

--- avalisystem/urls.py
+++ avalisystem/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.urls import path, include
+from django.contrib import admin
 
 from api import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+
     url(r'^$', views.index, name='index'),
 ]
